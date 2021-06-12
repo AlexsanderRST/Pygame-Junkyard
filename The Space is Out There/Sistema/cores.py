e7e3e2fe01643e9ae14d1706093617a9c56d6163
Alexsander Rosante <35708503+AlexsanderRST@users.noreply.github.com>
2021-06-12 20:13:02 -0300

    Add files via upload

---
+++ The Space is Out There/Sistema/cores.py
@@ -0,0 +1,16 @@
+# Criado por Alexsander Rosante
+
+preto = (0, 0, 0)
+branco = (255, 255, 255)
+vermelho = (255, 0, 0)
+verde = (0, 255, 0)
+azul = (0, 0, 255)
+amarelo = (0, 255, 255)
+rosa = (255, 0, 255)
+dimgray = (105, 105, 105)
+bluesky = (135, 206, 235)
+# Usados na textura de terra marrom:
+marrom1 = (179, 134, 92)
+marrom2 = (147, 109, 72)
+marrom3 = (119, 86, 53)
+cinza1 = (105, 107, 104)
---
+++ The Space is Out There/Sistema/parametros.py
@@ -0,0 +1,14 @@
+# Criado por Alexsander Rosante
+
+from Sistema.cores import *
+from math import *
+
+tela_largura, tela_altura = 1366, 768
+tela_centro = (tela_largura//2, tela_altura//2)
+aceleracao_gravidade = 10
+fundo_largura, fundo_altura = 2400, 1600
+cor_do_fundo = bluesky
+# Pixeis
+tamanho_do_pixel = 5
+pixeis_por_bloco = sqrt(256)
+limite_de_pixeis = 256
